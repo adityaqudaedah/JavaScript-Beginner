4c099f4fd4c6149e62feb847e86b84c437860a09
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2021-03-03 13:50:34 +0700

    adding function to higherOrderFunction.js

--- js/higherOrderFunction.js
+++ js/higherOrderFunction.js
@@ -17,3 +17,43 @@ function greatherThan(n) {
 
 let greaterThan10 = greatherThan(10)
 console.log(greaterThan10(11));
+
+//higher order function
+function noisy(f) {
+    return (...args)=>{
+        console.log("calling with",args);
+        let result = f(...args)
+        console.log("called with",args,", returned",result);
+        return result
+    }
+}
+/**
+ * function called below means passed the function noisy with 
+ * Math.min and also passed function inside the noisy function
+ * with (3,2,1)
+ * f(...args) it means that f refers to the Math.min and (...args)
+ * and the result is min value of the (...args)
+ */
+noisy(Math.min)(3,2,1)
+
+function unless(test,then) {
+    if(!test) then();
+}
+repeat(3,n => {
+    unless(n%2==1,() => {
+        console.log(n," is even");
+    })
+})
+
+//filtering array
+function filter(array, test) {
+    let passed = []
+    for(let element of array){
+        if(test(element)){}
+        passed.push(element)
+    }
+    return passed
+}
+
+console.log(filter(SCRIPTS, script => script.living));
+console.log(SCRIPTS.filter(s=> s.direction=="tbtb"));
