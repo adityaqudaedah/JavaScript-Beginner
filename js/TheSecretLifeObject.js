fbbb5e14906e47887d8acb56aa91f38a7311b003
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2021-03-08 17:02:10 +0700

    adding function

---
+++ js/TheSecretLifeObject.js
@@ -0,0 +1,55 @@
+// encapsulation
+let rabbit ={}
+Rabbit.speak = function (line){
+    console.log(`The rabbit says '${line}'`)
+}
+Rabbit.speak("hello mother fucker")
+
+function speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`)
+}
+let hungryRabbit = {type: "hungry",speak}
+let whiteRabbit = {type: "white", speak}
+
+whiteRabbit.speak("I am the one")
+hungryRabbit.speak("i could use carrot right now")
+
+speak.call(whiteRabbit,"Hello mother fucker wold")
+
+
+function normalize() {
+    console.log(this.coords.map(n=>n/this.length))
+}
+
+normalize.call({coords:[0,2,3], length: 5})
+
+console.log(Object.prototype)
+console.log(Object.getPrototypeOf(Object.prototype))
+
+let protoRabbit = {
+    speak(line){
+        console.log(`The ${this.type}, rabbit says '${line}'`)
+    }
+}
+let killerRabbit = Object.create(protoRabbit)
+killerRabbit.type = "Killer"
+killerRabbit.speak("SKREEE")
+
+console.log(Function.prototype)
+
+function makeRabbit(type){
+    let rabbit = Object.create(protoRabbit)
+    rabbit.type = type
+    return rabbit
+}
+
+//function bellow treated as a contructor
+function Rabbit(type) {
+    this.type = type
+}
+Rabbit.prototype.speak = function(word) {
+    console.log(`The ${this.type} rabbit says '${word}'`)
+}
+
+let weirdRabbit = new Rabbit("Weird")
+weirdRabbit.speak("Y'all bitches")
