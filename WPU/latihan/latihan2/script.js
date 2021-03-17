b78e0f288269bb012661e1c18b8c8f719932f0e4
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2021-03-18 00:24:24 +0700

    doing changes

--- WPU/latihan/latihan2/script.js
+++ WPU/latihan/latihan2/script.js
@@ -33,6 +33,51 @@
 
 // </div>`
 
+// conditionals (ternary operation)
+// const lagu = {
+//     penyanyi: 'Isyana Sarasvati',
+//     judul: 'Kau adalah',
+//     feat: 'Rayi putra'
+    
+// }
 
+// const el = `<div class="lagu">
+
+//     <ul>
+//         <li>${lagu.penyanyi}</li>
+//         <li>${lagu.judul} ${lagu.feat ?`(feat. ${lagu.feat})`:''}</li>
+//     </ul>
+
+// </div>`
+
+// nested html fragment
+const mhs = {
+    nama: 'Rahmat Aditya Qudaedah',
+    semester: 6,
+    matKul : [
+        'Bimbingan penulisan ilmiah : 2',
+        'Sistem Basis Data : 3',
+        'Konsep Data Mining : 2',
+        'Desain & Manaj. jaringan Komputer : 3'
+    ]
+}
+
+
+
+const el = `<div class="mhs">
+    <h2>${mhs.nama}</h2>
+    <h3>Semester : ${mhs.semester}</h3>
+    <h4>Mata Kuliah : </h4>
+    <span>
+        <ol>
+            ${mhs.matKul.map(items => {
+                const item = items.split(':')
+                return `<li>${item[0]}: ${parseInt(item[1])} SKS</li>`
+            }).join(' ')}
+        </ol>
+    </span>
+</div>`
 //push el to the html document
 document.body.innerHTML = el
+
+console.log(el)
